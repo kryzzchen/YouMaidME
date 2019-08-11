2789e2ce177bf79c8f2c530b21c89ab3fe9786cb
royeth <royethgomeseria1998@gmail.com>
2019-08-11 22:28:25 +0800

    Commit

--- app/src/main/java/com/example/youmaidme/LogIn_Activity.java
+++ app/src/main/java/com/example/youmaidme/LogIn_Activity.java
@@ -1,6 +1,9 @@
 package com.example.youmaidme;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,5 +14,14 @@ public class LogIn_Activity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        TextView login_activity_create_new_account_tv = (TextView) findViewById(R.id.login_activity_create_new_account_tv);
+
+        login_activity_create_new_account_tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LogIn_Activity.this, SignUp_Activity.class));
+            }
+        });
     }
 }
