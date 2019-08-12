172295f7ef87f47ae7b474db7e56abab18b4cb58
royeth <royethgomeseria1998@gmail.com>
2019-08-12 23:49:39 +0800

    With Firebase Setup; Toolbar added

--- app/src/main/java/com/example/youmaidme/LogIn_Activity.java
+++ app/src/main/java/com/example/youmaidme/LogIn_Activity.java
@@ -6,7 +6,9 @@ import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class LogIn_Activity extends AppCompatActivity {
 
@@ -14,6 +16,13 @@ public class LogIn_Activity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.login_activity_toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar action_bar = getSupportActionBar();
+
+        action_bar.setDisplayHomeAsUpEnabled(true);
 
         TextView login_activity_create_new_account_tv = (TextView) findViewById(R.id.login_activity_create_new_account_tv);
 
--- app/src/main/java/com/example/youmaidme/SignUp_Activity.java
+++ app/src/main/java/com/example/youmaidme/SignUp_Activity.java
@@ -3,7 +3,9 @@ package com.example.youmaidme;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class SignUp_Activity extends AppCompatActivity {
 
@@ -11,5 +13,12 @@ public class SignUp_Activity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_activity);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.signup_activity_toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar action_bar = getSupportActionBar();
+
+        action_bar.setDisplayHomeAsUpEnabled(true);
     }
 }
