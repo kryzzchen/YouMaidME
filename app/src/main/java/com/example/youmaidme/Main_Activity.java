ac47fa126320b5f2b68753535123500eb5ef82c7
royeth <royethgomeseria1998@gmail.com>
2019-08-21 13:48:43 +0800

    Sign Up; Log In, and Dashboard Activity Added

--- app/src/main/java/com/example/youmaidme/Main_Activity.java
+++ app/src/main/java/com/example/youmaidme/Main_Activity.java
@@ -7,28 +7,45 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Main_Activity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Main_Activity extends AppCompatActivity implements View.OnClickListener{
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+
+    private TextView activity_main_employer_click;
+    private TextView activity_main_maid_click;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-        TextView activity_main_employer_click = (TextView) findViewById(R.id.activity_main_employer_tv);
-        TextView activity_main_maid_click = (TextView) findViewById(R.id.activity_main_maid_tv);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
 
-        activity_main_employer_click.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(Main_Activity.this, LogIn_Activity.class));
-            }
-        });
+        if(firebaseUser != null){
+            startActivity(new Intent(this, Dashboard_Activity.class));
+        }
 
-        activity_main_maid_click.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(Main_Activity.this, LogIn_Activity.class));
-            }
-        });
+        activity_main_employer_click = (TextView) findViewById(R.id.activity_main_employer_tv);
+        activity_main_maid_click = (TextView) findViewById(R.id.activity_main_maid_tv);
+
+        activity_main_employer_click.setOnClickListener(this);
+        activity_main_maid_click.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == activity_main_employer_click){
+            finish();
+            startActivity(new Intent(Main_Activity.this, LogIn_Activity.class));
+        }
+        if(view == activity_main_maid_click){
+            finish();
+            startActivity(new Intent(Main_Activity.this, LogIn_Activity.class));
+        }
     }
 }
